124092ca5572f3d2ca2dfbe1e73e86580d594e76
Nick Linker <nlinker@gmail.com>
2021-04-30 10:00:40 +0600

    Reformat a bit
    Signed-off-by: Nick Linker <nlinker@gmail.com>

--- library/src/lib.h
+++ library/src/lib.h
@@ -12,57 +12,61 @@
 int cpp_test();
 
 // api-specific stuff below
-std::unique_ptr <AIModuleWrapper> createAIModuleWrapper(rust::Box<BoxedAIModule> box);
+std::unique_ptr <AIModuleWrapper> createAIModuleWrapper(rust::Box <BoxedAIModule> box);
 
 class AIModuleWrapper : public BWAPI::AIModule {
 public:
     BoxedAIModule *moduleBox;
 public:
-    AIModuleWrapper(BoxedAIModule *box): moduleBox(box)  {}
-    BoxedAIModule& getBox() { return *moduleBox; }
+    AIModuleWrapper(BoxedAIModule *box) : moduleBox(box) {}
 
-    void onStart() noexcept override { on_start(*this); }
-    void onEnd(bool isWinner) noexcept override { on_end(*this, isWinner); }
-    void onFrame() noexcept override { on_frame(*this); }
-    void onSendText(std::string text) noexcept override { on_send_text(*this, text); }
-    void onReceiveText(BWAPI::Player player, std::string text) noexcept override { on_receive_text(*this, player, text); }
-    void onPlayerLeft(BWAPI::Player player) noexcept override { on_player_left(*this, player); }
-    void onNukeDetect(BWAPI::Position target) noexcept override { on_nuke_detect(*this, target); }
-    void onUnitDiscover(BWAPI::Unit unit) noexcept override { on_unit_discover(*this, unit); }
-    void onUnitEvade(BWAPI::Unit unit) noexcept override { on_unit_evade(*this, unit); }
-    void onUnitShow(BWAPI::Unit unit) noexcept override { on_unit_show(*this, unit); }
-    void onUnitHide(BWAPI::Unit unit) noexcept override { on_unit_hide(*this, unit); }
-    void onUnitCreate(BWAPI::Unit unit) noexcept override { on_unit_create(*this, unit); }
-    void onUnitDestroy(BWAPI::Unit unit) noexcept override { on_unit_destroy(*this, unit); }
-    void onUnitMorph(BWAPI::Unit unit) noexcept override { on_unit_morph(*this, unit); }
-    void onUnitRenegade(BWAPI::Unit unit) noexcept override { on_unit_renegade(*this, unit); }
-    void onSaveGame(std::string gameName) noexcept override { on_save_game(*this, gameName); }
-    void onUnitComplete(BWAPI::Unit unit) noexcept override { on_unit_complete(*this, unit); }
+    BoxedAIModule &getBox() { return *moduleBox; }
+
+    void onStart() noexcept override{on_start(*this);}
+    void onEnd(bool isWinner) noexcept override{on_end(*this, isWinner);}
+    void onFrame() noexcept override{on_frame(*this);}
+    void onSendText(std::string text) noexcept override{on_send_text(*this, text);}
+    void onReceiveText(BWAPI::Player player, std::string text) noexcept override{on_receive_text(*this, player, text);}
+    void onPlayerLeft(BWAPI::Player player) noexcept override{on_player_left(*this, player);}
+    void onNukeDetect(BWAPI::Position target) noexcept override{on_nuke_detect(*this, target);}
+    void onUnitDiscover(BWAPI::Unit unit) noexcept override{on_unit_discover(*this, unit);}
+    void onUnitEvade(BWAPI::Unit unit) noexcept override{on_unit_evade(*this, unit);}
+    void onUnitShow(BWAPI::Unit unit) noexcept override{on_unit_show(*this, unit);}
+    void onUnitHide(BWAPI::Unit unit) noexcept override{on_unit_hide(*this, unit);}
+    void onUnitCreate(BWAPI::Unit unit) noexcept override{on_unit_create(*this, unit);}
+    void onUnitDestroy(BWAPI::Unit unit) noexcept override{on_unit_destroy(*this, unit);}
+    void onUnitMorph(BWAPI::Unit unit) noexcept override{on_unit_morph(*this, unit);}
+    void onUnitRenegade(BWAPI::Unit unit) noexcept override{on_unit_renegade(*this, unit);}
+    void onSaveGame(std::string gameName) noexcept override{on_save_game(*this, gameName);}
+    void onUnitComplete(BWAPI::Unit unit) noexcept override{on_unit_complete(*this, unit);}
 };
 
 using UnitFilter = rust::Fn<bool(BWAPI::Unit)>;
 using BestUnitFilter = rust::Fn<BWAPI::Unit(BWAPI::Unit, BWAPI::Unit)>;
 
 const BWAPI::UnitInterface *_unitset_getClosestUnit(const BWAPI::Unitset &set, UnitFilter pred, int radius);
-std::unique_ptr<UnitsetIterator> _unitset_getInterceptors(const BWAPI::Unitset &set);
-std::unique_ptr<UnitsetIterator> _unitset_getLarva(const BWAPI::Unitset &set);
-std::unique_ptr<UnitsetIterator> _unitset_getLoadedUnits(const BWAPI::Unitset &set);
-std::unique_ptr<UnitsetIterator> _unitset_getUnitsInRadius(const BWAPI::Unitset &set, int radius, UnitFilter pred);
+std::unique_ptr <UnitsetIterator> _unitset_getInterceptors(const BWAPI::Unitset &set);
+std::unique_ptr <UnitsetIterator> _unitset_getLarva(const BWAPI::Unitset &set);
+std::unique_ptr <UnitsetIterator> _unitset_getLoadedUnits(const BWAPI::Unitset &set);
+std::unique_ptr <UnitsetIterator> _unitset_getUnitsInRadius(const BWAPI::Unitset &set, int radius, UnitFilter pred);
 bool _unitset_move(const BWAPI::Unitset &set, BWAPI::Position target, bool shift_queue_command);
 
 void _game_debug(const BWAPI::Game &game);
 void _game_debug_fun(const BWAPI::Game &game, UnitFilter fun);
-BWAPI::UnitInterface* _game_getBestUnit(const BWAPI::Game &game, BestUnitFilter best, UnitFilter pred, BWAPI::Position center, int radius);
+BWAPI::UnitInterface *_game_getBestUnit(const BWAPI::Game &game, BestUnitFilter best, UnitFilter pred, BWAPI::Position center, int radius);
 BWAPI::UnitInterface *_game_getClosestUnit(const BWAPI::Game &game, BWAPI::Position center, UnitFilter pred, int radius);
 BWAPI::UnitInterface *_game_getClosestUnitInRectangle(const BWAPI::Game &game, BWAPI::Position center, UnitFilter pred, int left, int top, int right, int bottom);
-const EventList& _game_getEvents(const BWAPI::Game &game);
+const EventList &_game_getEvents(const BWAPI::Game &game);
 std::unique_ptr<UnitsetIterator> _game_getUnitsInRadius(const BWAPI::Game &game, BWAPI::Position position, int radius, UnitFilter pred);
 std::unique_ptr<UnitsetIterator> _game_getUnitsInRectangle(const BWAPI::Game &game, BWAPI::Position topLeft, BWAPI::Position bottomRight, UnitFilter pred);
 std::unique_ptr<UnitsetIterator> _game_getUnitsOnTile(const BWAPI::Game &game, BWAPI::TilePosition tile, UnitFilter pred);
-std::unique_ptr<std::string> _game_mapFileName(const BWAPI::Game& game);
-std::unique_ptr<std::string> _game_mapHash(const BWAPI::Game& game);
-std::unique_ptr<std::string> _game_mapName(const BWAPI::Game& game);
-std::unique_ptr<std::string> _game_mapPathName(const BWAPI::Game& game);
+std::unique_ptr<std::string> _game_mapFileName(const BWAPI::Game &game);
+std::unique_ptr<std::string> _game_mapHash(const BWAPI::Game &game);
+std::unique_ptr<std::string> _game_mapName(const BWAPI::Game &game);
+std::unique_ptr<std::string> _game_mapPathName(const BWAPI::Game &game);
+
+std::vector<int> theVector();
+
 void _game_printf(BWAPI::Game &game, rust::Str text);
 BWAPI::PlayerInterface *_game_self(const BWAPI::Game &game);
 void _game_sendText(BWAPI::Game &game, rust::Str text);
