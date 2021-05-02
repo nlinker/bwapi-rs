3c9a1cc3fd2f546be21462408834923451671a39
Nick Linker <nlinker@gmail.com>
2021-05-03 02:51:07 +0600

    Cleanup
    Signed-off-by: Nick Linker <nlinker@gmail.com>

--- library/src/lib.h
+++ library/src/lib.h
@@ -1,19 +1,19 @@
 #pragma once
 
-#include "aim.h"
-#include "aliases.h"
-#include "iterator.h"
 #include <BWAPI/AIModule.h>
 #include <BWAPI/Game.h>
 #include <BWAPI/Unit.h>
 #include <BWAPI/Position.h>
+#include "aim.h"
+#include "aliases.h"
+#include "iterator.h"
 #include "library/src/lib.rs.h"
-#include "../../target/debug/build/library-10a440775d1794e8/out/cxxbridge/include/library/src/lib.rs.h"
+//#include "../../target/debug/build/library-10a440775d1794e8/out/cxxbridge/include/library/src/lib.rs.h"
 
 int cpp_test();
 
 // api-specific stuff below
-std::unique_ptr <AIModuleWrapper> createAIModuleWrapper(rust::Box <BoxedAIModule> box);
+std::unique_ptr<AIModuleWrapper> createAIModuleWrapper(rust::Box<BoxedAIModule> box);
 
 class AIModuleWrapper : public BWAPI::AIModule {
 public:
@@ -23,23 +23,39 @@ public:
 
     BoxedAIModule &getBox() { return *moduleBox; }
 
-    void onStart() noexcept override{on_start(*this);}
-    void onEnd(bool isWinner) noexcept override{on_end(*this, isWinner);}
-    void onFrame() noexcept override{on_frame(*this);}
-    void onSendText(std::string text) noexcept override{on_send_text(*this, text);}
-    void onReceiveText(BWAPI::Player player, std::string text) noexcept override{on_receive_text(*this, player, text);}
-    void onPlayerLeft(BWAPI::Player player) noexcept override{on_player_left(*this, player);}
-    void onNukeDetect(BWAPI::Position target) noexcept override{on_nuke_detect(*this, target);}
-    void onUnitDiscover(BWAPI::Unit unit) noexcept override{on_unit_discover(*this, unit);}
-    void onUnitEvade(BWAPI::Unit unit) noexcept override{on_unit_evade(*this, unit);}
-    void onUnitShow(BWAPI::Unit unit) noexcept override{on_unit_show(*this, unit);}
-    void onUnitHide(BWAPI::Unit unit) noexcept override{on_unit_hide(*this, unit);}
-    void onUnitCreate(BWAPI::Unit unit) noexcept override{on_unit_create(*this, unit);}
-    void onUnitDestroy(BWAPI::Unit unit) noexcept override{on_unit_destroy(*this, unit);}
-    void onUnitMorph(BWAPI::Unit unit) noexcept override{on_unit_morph(*this, unit);}
-    void onUnitRenegade(BWAPI::Unit unit) noexcept override{on_unit_renegade(*this, unit);}
-    void onSaveGame(std::string gameName) noexcept override{on_save_game(*this, gameName);}
-    void onUnitComplete(BWAPI::Unit unit) noexcept override{on_unit_complete(*this, unit);}
+    void onStart() noexcept override { on_start(*this); }
+
+    void onEnd(bool isWinner) noexcept override { on_end(*this, isWinner); }
+
+    void onFrame() noexcept override { on_frame(*this); }
+
+    void onSendText(std::string text) noexcept override { on_send_text(*this, text); }
+
+    void onReceiveText(BWAPI::Player player, std::string text) noexcept override { on_receive_text(*this, player, text); }
+
+    void onPlayerLeft(BWAPI::Player player) noexcept override { on_player_left(*this, player); }
+
+    void onNukeDetect(BWAPI::Position target) noexcept override { on_nuke_detect(*this, target); }
+
+    void onUnitDiscover(BWAPI::Unit unit) noexcept override { on_unit_discover(*this, unit); }
+
+    void onUnitEvade(BWAPI::Unit unit) noexcept override { on_unit_evade(*this, unit); }
+
+    void onUnitShow(BWAPI::Unit unit) noexcept override { on_unit_show(*this, unit); }
+
+    void onUnitHide(BWAPI::Unit unit) noexcept override { on_unit_hide(*this, unit); }
+
+    void onUnitCreate(BWAPI::Unit unit) noexcept override { on_unit_create(*this, unit); }
+
+    void onUnitDestroy(BWAPI::Unit unit) noexcept override { on_unit_destroy(*this, unit); }
+
+    void onUnitMorph(BWAPI::Unit unit) noexcept override { on_unit_morph(*this, unit); }
+
+    void onUnitRenegade(BWAPI::Unit unit) noexcept override { on_unit_renegade(*this, unit); }
+
+    void onSaveGame(std::string gameName) noexcept override { on_save_game(*this, gameName); }
+
+    void onUnitComplete(BWAPI::Unit unit) noexcept override { on_unit_complete(*this, unit); }
 };
 
 using UnitFilter = rust::Fn<bool(BWAPI::Unit)>;
@@ -58,8 +74,8 @@ BWAPI::UnitInterface *_game_getBestUnit(const BWAPI::Game &game, BestUnitFilter 
 BWAPI::UnitInterface *_game_getClosestUnit(const BWAPI::Game &game, BWAPI::Position center, UnitFilter pred, int radius);
 BWAPI::UnitInterface *_game_getClosestUnitInRectangle(const BWAPI::Game &game, BWAPI::Position center, UnitFilter pred, int left, int top, int right, int bottom);
 const EventList &_game_getEvents(const BWAPI::Game &game);
-rust::Vec<BWAPI::Position> _game_getNukeDots(const BWAPI::Game& game);
-rust::Vec<BWAPI::TilePosition> _game_getStartLocations(const BWAPI::Game& game);
+rust::Vec<BWAPI::Position> _game_getNukeDots(const BWAPI::Game &game);
+rust::Vec<BWAPI::TilePosition> _game_getStartLocations(const BWAPI::Game &game);
 std::unique_ptr<BWAPI::Unitset> _game_getUnitsInRadius(const BWAPI::Game &game, BWAPI::Position position, int radius, UnitFilter pred);
 std::unique_ptr<BWAPI::Unitset> _game_getUnitsInRectangle(const BWAPI::Game &game, BWAPI::Position topLeft, BWAPI::Position bottomRight, UnitFilter pred);
 std::unique_ptr<BWAPI::Unitset> _game_getUnitsOnTile(const BWAPI::Game &game, BWAPI::TilePosition tile, UnitFilter pred);
@@ -74,29 +90,29 @@ void _game_sendTextEx(BWAPI::Game &game, bool toAllies, rust::Str text);
 bool _game_setMap(BWAPI::Game &game, rust::Str text);
 void _game_drawText(BWAPI::Game &game, BWAPI::CoordinateType::Enum ctype, int x, int y, rust::Str text);
 
-std::unique_ptr<std::string> _player_getName(const BWAPI::PlayerInterface& player);
-BWAPI::Text::Enum _player_getTextColor(const BWAPI::PlayerInterface& player);
+std::unique_ptr<std::string> _player_getName(const BWAPI::PlayerInterface &player);
+BWAPI::Text::Enum _player_getTextColor(const BWAPI::PlayerInterface &player);
 
-std::unique_ptr<BWAPI::Unitset> _region_getUnits(const BWAPI::RegionInterface& region, UnitFilter pred);
+std::unique_ptr<BWAPI::Unitset> _region_getUnits(const BWAPI::RegionInterface &region, UnitFilter pred);
 
 int Unit_getId(const BWAPI::UnitInterface *unit);
 BWAPI::UnitType Unit_getType(const BWAPI::UnitInterface *unit);
 BWAPI::Position Unit_getPosition(const BWAPI::UnitInterface *unit);
 
-static_assert (sizeof(BWAPI::BulletType) == 4, "BWAPI::BulletType size is not correct");
-static_assert (sizeof(BWAPI::Color) == 4, "BWAPI::Color size is not correct");
-static_assert (sizeof(BWAPI::DamageType) == 4, "BWAPI::DamageType size is not correct");
-static_assert (sizeof(BWAPI::Error) == 4, "BWAPI::Error size is not correct");
-static_assert (sizeof(BWAPI::ExplosionType) == 4, "BWAPI::ExplosionType size is not correct");
-static_assert (sizeof(BWAPI::GameType) == 4, "BWAPI::GameType size is not correct");
-static_assert (sizeof(BWAPI::Order) == 4, "BWAPI::Order size is not correct");
-static_assert (sizeof(BWAPI::PlayerType) == 4, "BWAPI::PlayerType size is not correct");
-static_assert (sizeof(BWAPI::Race) == 4, "BWAPI::Race size is not correct");
-static_assert (sizeof(BWAPI::TechType) == 4, "BWAPI::TechType size is not correct");
-static_assert (sizeof(BWAPI::UnitCommand) == 40, "BWAPI::UnitCommand size is not correct");
-static_assert (sizeof(BWAPI::UnitCommandType) == 4, "BWAPI::UnitCommandType size is not correct");
-static_assert (sizeof(BWAPI::UnitSizeType) == 4, "BWAPI::UnitSizeType size is not correct");
-static_assert (sizeof(BWAPI::UnitType) == 4, "BWAPI::UnitType size is not correct");
-static_assert (sizeof(BWAPI::UpgradeType) == 4, "BWAPI::UpgradeType size is not correct");
-static_assert (sizeof(BWAPI::WeaponType) == 4, "BWAPI::WeaponType size is not correct");
-static_assert (sizeof(BWAPI::Text::Size::Enum) == 4, "BWAPI::Text::Size::Enum size is not correct");
+static_assert(sizeof(BWAPI::BulletType) == 4, "BWAPI::BulletType size is not correct");
+static_assert(sizeof(BWAPI::Color) == 4, "BWAPI::Color size is not correct");
+static_assert(sizeof(BWAPI::DamageType) == 4, "BWAPI::DamageType size is not correct");
+static_assert(sizeof(BWAPI::Error) == 4, "BWAPI::Error size is not correct");
+static_assert(sizeof(BWAPI::ExplosionType) == 4, "BWAPI::ExplosionType size is not correct");
+static_assert(sizeof(BWAPI::GameType) == 4, "BWAPI::GameType size is not correct");
+static_assert(sizeof(BWAPI::Order) == 4, "BWAPI::Order size is not correct");
+static_assert(sizeof(BWAPI::PlayerType) == 4, "BWAPI::PlayerType size is not correct");
+static_assert(sizeof(BWAPI::Race) == 4, "BWAPI::Race size is not correct");
+static_assert(sizeof(BWAPI::TechType) == 4, "BWAPI::TechType size is not correct");
+static_assert(sizeof(BWAPI::UnitCommand) == 40, "BWAPI::UnitCommand size is not correct");
+static_assert(sizeof(BWAPI::UnitCommandType) == 4, "BWAPI::UnitCommandType size is not correct");
+static_assert(sizeof(BWAPI::UnitSizeType) == 4, "BWAPI::UnitSizeType size is not correct");
+static_assert(sizeof(BWAPI::UnitType) == 4, "BWAPI::UnitType size is not correct");
+static_assert(sizeof(BWAPI::UpgradeType) == 4, "BWAPI::UpgradeType size is not correct");
+static_assert(sizeof(BWAPI::WeaponType) == 4, "BWAPI::WeaponType size is not correct");
+static_assert(sizeof(BWAPI::Text::Size::Enum) == 4, "BWAPI::Text::Size::Enum size is not correct");
